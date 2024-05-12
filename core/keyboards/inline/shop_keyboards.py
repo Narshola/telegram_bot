d8090ed20aa3fab1f87402ae24fd8361db7c637c
Narshola <100492751+Narshola@users.noreply.github.com>
2024-05-12 15:08:46 +0300

    Add files via upload

--- core/keyboards/inline/shop_keyboards.py
+++ core/keyboards/inline/shop_keyboards.py
@@ -12,24 +12,31 @@ def get_low_price_kb():
         )
     )
     kb.button(
-        text='Попсокет',
+        text='Стикер',
         callback_data=LowPriceProduct(
-            name='Попсокет',
-            price=100,
+            name='Стикер',
+            price=50,
             lucky_sale=True
         )
     )
-    kb.adjust(1)
+    kb.adjust(2)
     return kb.as_markup()
 
 def get_high_price_kb():
     kb = InlineKeyboardBuilder()
     kb.button(
-        text='Дорогой попсокет',
+        text='Клавиатура',
         callback_data=HighPriceProduct(
-            name='Попсокет',
-            price=105
+            name='Клавиатура',
+            price=1000
         )
     )
-    kb.adjust(1)
+    kb.button(
+        text='Наушники',
+        callback_data=HighPriceProduct(
+            name='Наушники',
+            price=800
+        )
+    )
+    kb.adjust(2)
     return kb.as_markup()
