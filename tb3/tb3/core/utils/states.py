df3604b8e6f4877d038091b7e8e7dbe5202f1fec
Narshola <100492751+Narshola@users.noreply.github.com>
2024-05-12 14:33:29 +0300

    Delete tb3/tb3 directory

--- tb3/tb3/core/utils/states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-
-class PlayerRegistrationState(StatesGroup):
-    hero_name = State()
-    hero_race = State()
-    hero_class = State()
-    hero_avatar = State()
